b291faf11a8bcf8d5722d5739886c3997ef90a88
Michael Chase <mchase62@byu.edu>
2024-02-26 18:23:48 -0700

    More Registration edits

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -2,14 +2,20 @@ package server;
 
 import com.google.gson.Gson;
 import dataAccess.DataAccessException;
+import dataAccess.UserDAO;
 import service.UserService;
 import spark.*;
 import service.ClearService;
 import model.UserData;
+
+import java.util.ArrayList;
+
 public class Server {
     private final ClearService clearService = new ClearService();
-    private final UserService userService;
-
+    private final UserService userService = new UserService();
+    private ArrayList<UserData> users = new ArrayList<>();
+    public Server() {
+    }
 
 
     public int run(int desiredPort) {
@@ -41,6 +47,7 @@ public class Server {
     private Object register(Request req, Response res) throws DataAccessException {
         var user = new Gson().fromJson(req.body(), UserData.class);
         user = userService.register(user);
+        users.add(user);
         return new Gson().toJson(user);
     }
 
--- server/src/main/java/server/UserHandler.java
+++ server/src/main/java/server/UserHandler.java
@@ -1,14 +1,19 @@
 package server;
 
+import dataAccess.DataAccessException;
 import service.UserService;
 import spark.Request;
 import spark.Response;
-
+import model.UserData;
 public class UserHandler {
     UserService UserService = new UserService();
 
     public Object register(Request req, Response res) {
-        UserService.register();
+        try {
+            UserService.register(new UserData(req.params(":user"), req.params(":password"), req.params(":email")));
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
         res.status(200);
         return "{}";
     }
