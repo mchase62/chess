06f96b3a1e0771b8f687800ba732c5da26261017
Michael Chase <mchase62@byu.edu>
2024-03-06 16:56:38 -0700

    Clear Auth Passed

--- server/src/test/java/dataAccessTests/SQLAuthDAOTest.java
+++ server/src/test/java/dataAccessTests/SQLAuthDAOTest.java
@@ -115,7 +115,7 @@ public class SQLAuthDAOTest {
 
     @ParameterizedTest
     @ValueSource(classes = {SQLAuthDAO.class, MemoryAuthDAO.class})
-    void deleteAuthTestFail(Class<? extends AuthDAO> authDAOClass) throws DataAccessException {
+    void deleteAuthTestFail(Class<? extends AuthDAO> authDAOClass) throws DataAccessException { // deletes using wrong auth token
         AuthDAO authDAO = getAuthDAO(authDAOClass);
         AuthData returnedAuth;
         String status;
@@ -150,5 +150,112 @@ public class SQLAuthDAOTest {
             userDAO.clear();
             assertEquals("fail", status);
         }
+    }
+    @ParameterizedTest
+    @ValueSource(classes = {SQLAuthDAO.class, MemoryAuthDAO.class})
+    void getUserTestSuccess(Class<? extends AuthDAO> authDAOClass) throws DataAccessException {
+        AuthDAO authDAO = getAuthDAO(authDAOClass);
+        AuthData returnedAuth;
+        String returnedUser;
+        var user = new UserData("auth_username", "auth_password", "auth_email");
+
+        if (SQLAuthDAO.class.isAssignableFrom(authDAOClass)) { // run SQL test
+            // add user to user database
+            var createUserStatement = "INSERT INTO user (username, password, email) values (?, ?,?) ";
+            DatabaseManager.executeUpdate(createUserStatement, user.username(), user.password(), user.email());
+
+            // create auth for created user
+            returnedAuth = authDAO.createAuth(user.username());
+
+            returnedUser = authDAO.getUser(returnedAuth.authToken());
+            // delete user from user database
+            var deleteUserStatement = "DELETE FROM user";
+
+            DatabaseManager.executeUpdate(deleteUserStatement);
+        }
+        else {
+            // add user to memory
+            MemoryUserDAO userDAO = MemoryUserDAO.getInstance();
+            userDAO.createUser(user);
+
+            returnedAuth = authDAO.createAuth(user.username());
+
+            returnedUser = authDAO.getUser(returnedAuth.authToken()); // access user with auth token
+
+            userDAO.clear(); // delete user from memory
+        }
+        assertEquals(user.username(), returnedUser);
+    }
+
+    @ParameterizedTest
+    @ValueSource(classes = {SQLAuthDAO.class, MemoryAuthDAO.class})
+    void getUserTestFail(Class<? extends AuthDAO> authDAOClass) throws DataAccessException { // attempts to get user with a nonexistent token
+        AuthDAO authDAO = getAuthDAO(authDAOClass);
+        AuthData returnedAuth;
+        String returnedUser;
+        var user = new UserData("auth_username", "auth_password", "auth_email");
+
+        if (SQLAuthDAO.class.isAssignableFrom(authDAOClass)) { // run SQL test
+            // add user to user database
+            var createUserStatement = "INSERT INTO user (username, password, email) values (?, ?,?) ";
+            DatabaseManager.executeUpdate(createUserStatement, user.username(), user.password(), user.email());
+
+            // create auth for created user
+            authDAO.createAuth(user.username());
+
+            returnedUser = authDAO.getUser("not_a_token");
+            // delete user from user database
+            var deleteUserStatement = "DELETE FROM user";
+            DatabaseManager.executeUpdate(deleteUserStatement);
+        }
+        else {
+            // add user and auth to memory
+            MemoryUserDAO userDAO = MemoryUserDAO.getInstance();
+            userDAO.createUser(user);
+            authDAO.createAuth(user.username());
+
+            returnedUser = authDAO.getUser("not_a_token"); // access user with fake token
+
+            userDAO.clear(); // delete user from memory
+        }
+        assertNull(returnedUser);
+    }
+
+    @ParameterizedTest
+    @ValueSource(classes = {SQLAuthDAO.class, MemoryAuthDAO.class})
+    void clearTestSuccess(Class<? extends AuthDAO> authDAOClass) throws DataAccessException { // attempts to get user with a nonexistent token
+        AuthDAO authDAO = getAuthDAO(authDAOClass);
+        AuthData returnedAuth;
+        String returnedUser;
+        var user = new UserData("auth_username", "auth_password", "auth_email");
+
+        if (SQLAuthDAO.class.isAssignableFrom(authDAOClass)) { // run SQL test
+            // add user to user database
+            var createUserStatement = "INSERT INTO user (username, password, email) values (?, ?,?) ";
+            DatabaseManager.executeUpdate(createUserStatement, user.username(), user.password(), user.email());
+
+            // create auth for created user
+            returnedAuth = authDAO.createAuth(user.username());
+
+            // delete user from user database
+            var deleteUserStatement = "DELETE FROM user";
+
+            authDAO.clear(); // delete from auth table
+            DatabaseManager.executeUpdate(deleteUserStatement);
+            returnedUser = authDAO.getUser(returnedAuth.authToken());
+        }
+        else {
+            // add user to memory
+            MemoryUserDAO userDAO = MemoryUserDAO.getInstance();
+            userDAO.createUser(user);
+
+            returnedAuth = authDAO.createAuth(user.username());
+
+            authDAO.clear();
+            // delete user from memory
+            userDAO.clear();
+            returnedUser = authDAO.getUser(returnedAuth.authToken());
+        }
+        assertNull(returnedUser);
     }
 }
