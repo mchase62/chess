05fac7c92b0c186489b2b92331d3f33b63fb9dd5
Michael Chase <mchase62@byu.edu>
2024-03-20 22:24:18 -0600

    Repl

--- client/src/main/java/client/ChessClient.java
+++ client/src/main/java/client/ChessClient.java
@@ -1,6 +1,16 @@
 package client;
 
 import server.ServerFacade;
-public class ChessClient {
+import client.websocket.NotificationHandler;
 
+public class ChessClient {
+    private final ServerFacade server;
+    private final String serverUrl;
+    private final NotificationHandler notificationHandler;
+
+    public ChessClient(String serverUrl, NotificationHandler notificationHandler) {
+        server = new ServerFacade(serverUrl);
+        this.serverUrl = serverUrl;
+        this.notificationHandler = notificationHandler;
+    }
 }
---
+++ client/src/main/java/client/Repl.java
@@ -0,0 +1,11 @@
+package client;
+
+import client.websocket.NotificationHandler;
+
+public class Repl implements NotificationHandler {
+    private final ChessClient client;
+
+    public Repl(String serverUrl) {
+        client = new ChessClient(serverUrl, this);
+    }
+}
